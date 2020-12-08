ce3351f59223ba41eed47791ae961b0aae0cc47b
ssrful <ssrful@gmail.com>
2020-12-08 14:58:18 -0500

    new attp, to see if pass

--- src/main/lister.py
+++ src/main/lister.py
@@ -1,14 +1,6 @@
 # Created by Leon Hunter at 12:10 PM 12/08/2020
 class Lister(object):
     def get_integer_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-        '''
         integerList = []
         while start <= stop:
             if step == 0:
@@ -19,15 +11,6 @@ class Lister(object):
         return integerList
 
     def get_even_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-            and are divisible by 2
-        '''
         evenList = []
         while start <= stop:
             if step == 0:
@@ -41,15 +24,6 @@ class Lister(object):
         return evenList
 
     def get_odd_list(self, start, stop, step):
-        ''' TODO - Implement solution
-        Given 3 integers,
-            `start`, `stop`, and `step`
-        return a list of all integers
-            between `start`,
-            incrementing by `step`,
-            up to and including `stop`
-            and are not divisible by 2
-        '''
         oddList = []
         while start <= stop:
             if step = 0:
